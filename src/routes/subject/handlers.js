f882232aa8a25b30d247b9a3d1f61832cc6903a9
robert <robertcumahig@gmail.com>
2022-10-28 06:50:32 +0800

    update

--- src/routes/subject/handlers.js
+++ src/routes/subject/handlers.js
@@ -60,4 +60,14 @@ internals.delete_subjects = async (req, res) => {
   console.log(r);
   return res.response({ message: "success" }).code(200);
 };
+internals.edit_subject = async (req, res) => {
+  const updatorId = req.auth.credentials._id;
+  const id = req.params.id;
+  const filter = { _id: id };
+  const payload = { ...req.payload, updatorId };
+
+  let r = await Subjects.findOneAndUpdate(filter, payload);
+  console.log(r);
+  return res.response({ message: "success" }).code(200);
+};
 module.exports = internals;
