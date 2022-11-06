d28e36de7bc8f3d1c15d83c86a268a8a56a45aff
robert <robertcumahig@gmail.com>
2022-11-06 14:17:49 +0800

    update

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -51,7 +51,7 @@ const UserSchema = new mongoose.Schema(
     suffix: { type: String },
     gradeLevel: { type: String },
     role: { type: Array },
-    idNo: { type: String, required: true },
+    idNo: { type: String },
     phoneNumber: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String },
