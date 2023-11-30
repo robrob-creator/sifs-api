ab161b04d79a4744a2f21160e48d4e2f3d2ba835
robert <robertcumahig@gmail.com>
2023-11-30 18:59:22 +0800

    update

--- src/database/models/Tests.js
+++ src/database/models/Tests.js
@@ -3,10 +3,106 @@ const Schema = mongoose.Schema;
 
 var TestSchema = new Schema(
   {
-    results: {
-      type: Object,
+    line: {
+      type: Number,
       required: true,
     },
+    elements: [
+      {
+        start_timestamp: {
+          type: Date,
+          required: true,
+        },
+        line: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        description: {
+          type: String,
+          required: true,
+        },
+        id: {
+          type: String,
+          required: true,
+        },
+        type: {
+          type: String,
+          required: true,
+        },
+        keyword: {
+          type: String,
+          required: true,
+        },
+        steps: [
+          {
+            result: {
+              status: {
+                type: String,
+                required: true,
+              },
+            },
+            line: {
+              type: Number,
+              required: true,
+            },
+            name: {
+              type: String,
+              required: true,
+            },
+            match: {
+              location: {
+                type: String,
+                required: true,
+              },
+            },
+            keyword: {
+              type: String,
+              required: true,
+            },
+          },
+        ],
+        tags: [
+          {
+            name: {
+              type: String,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    keyword: {
+      type: String,
+      required: true,
+    },
+    uri: {
+      type: String,
+      required: true,
+    },
+    tags: [
+      {
+        name: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
   },
   { timestamps: true }
 );
