b24cc1c513218bd57d2930665549e3b1b440fad8
robert <robertcumahig@gmail.com>
2023-11-30 17:40:00 +0800

    update

--- src/config/routes.js
+++ src/config/routes.js
@@ -3,10 +3,23 @@
 var internals = {};
 
 // Endpoints
-
+let Student = require("../routes/user/endpoints");
+let Main = require("../routes/main/endpoints");
+let Subject = require("../routes/subject/endpoints");
+let Section = require("../routes/section/endpoints");
+let Grade = require("../routes/grade/endpoints");
+let Feedback = require("../routes/feedback/endpoints");
 let Test = require("../routes/Test/endpoints");
 
-internals.routes = [...Test.endpoints];
+internals.routes = [
+  ...Student.endpoints,
+  ...Main.endpoints,
+  ...Subject.endpoints,
+  ...Section.endpoints,
+  ...Grade.endpoints,
+  ...Feedback.endpoints,
+  ...Test.endpoints,
+];
 
 internals.init = function (server) {
   server.route(internals.routes);
